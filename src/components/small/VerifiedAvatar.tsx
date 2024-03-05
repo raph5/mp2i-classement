8db8dbf0c1a14ada47b62eef70fd80e6eb7e74ad
raph5 <raphguyader@gmail.com>
2024-03-05 22:10:53 +0100

    small cosmetic update

--- src/components/small/VerifiedAvatar.tsx
+++ src/components/small/VerifiedAvatar.tsx
@@ -10,7 +10,7 @@ const VerifiedAvatar: React.FC<VerifiedAvatarProps> = (props) => {
   return (
     <div className="relative">
       {props.verified &&
-        <span className="material-symbols-rounded absolute z-20 text-success text-lg leading-5 top-1 right-1">
+        <span className="material-symbols-rounded absolute z-20 text-success text-lg leading-5 top-1 right-1 drop-shadow">
           verified
         </span>
       }
